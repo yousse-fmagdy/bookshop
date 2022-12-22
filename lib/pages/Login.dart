8fcd98a1abc0ac1548528cdb5fb7095521c40c9f
yousse-fmagdy <youssefmagdy1676@gmail.com>
2022-12-22 21:37:56 +0200

    add Sign Button

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bookshop/shaired/colors.dart';
 import 'package:bookshop/shaired/text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -29,24 +30,37 @@ class Login extends StatelessWidget {
               ispassword: true,
               hinttexxt: "Please Enter Your password ",
             ),
-
-
-SizedBox(
+            SizedBox(
               height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(BTNbrown),
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8))),
+              ),
+              child: Text(
+                "Sign in",
+                style: TextStyle(fontSize: 18),
+              ),
             ),
 
 
-ElevatedButton(
+ SizedBox(
+              height: 30,
+            ),
+
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Text("Do Not have An Account?" , style: TextStyle(fontSize: 20)),TextButton(
    onPressed: (){},
-   style: ButtonStyle(
-     backgroundColor: MaterialStateProperty.all(Colors.orange),
-     padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-     shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-  ),
-   child: Text("click here", style: TextStyle(fontSize: 19),),
-),
-
-
+   
+  child: Text('sign up', style: TextStyle(fontSize: 20)),)
+  ],
+)
 
 
           ],
---
+++ lib/shaired/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const BTNbgreen = Color.fromARGB(255, 39, 241, 123);
+ const BTNbrown = Color.fromARGB(26, 83, 71, 7);
+ const appbarbrown = Color.fromARGB(41, 65, 82, 3);
+
