8279386158db28c927831231f9ee6be69813cef7
yousse-fmagdy <youssefmagdy1676@gmail.com>
2022-12-22 19:54:30 +0200

    add TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import 'package:bookshop/pages/Login.dart';
 import 'package:flutter/material.dart';
 
+import 'pages/Login.dart';
+void main() {
+  runApp(const MyApp());
+}
+ 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
+ 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
+    return const MaterialApp(
       home: Login(),
     );
   }
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -7,7 +8,34 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Padding(
+        padding: const EdgeInsets.all(830.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Your Password : ",
+                  // To delete borders
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: Divider.createBorderSide(context),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  // fillColor: Colors.red,
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(8),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
