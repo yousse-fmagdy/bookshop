618dce2473707c165360d8db73ee5b1d2bcd563b
yousse-fmagdy <youssefmagdy1676@gmail.com>
2022-12-24 01:30:53 +0200

    product_price

--- lib/pages/homr.dart
+++ lib/pages/homr.dart
@@ -118,36 +118,9 @@ class Home extends StatelessWidget {
           // ignore: prefer_const_literals_to_create_immutables
           actions: [
             Consumer<Cart>(builder: ((context, classInstancee, child) {
-              return Row(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                          child: Text(
-                            "${classInstancee.selectedproducts.length} ",
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: Color.fromARGB(255, 0, 0, 0)),
-                          ),
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(211, 164, 255, 193),
-                              shape: BoxShape.circle)),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.add_shopping_cart)),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15),
-                    child: Text(
-                      "\$ 128",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              );
-            })),
+             
+
+             //here 
           ],
 
           backgroundColor: appbarbrown,
---
+++ lib/shaired/appbar.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class product_price extends StatelessWidget {
+  const product_price({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
