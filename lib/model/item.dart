b8e457af99361e97031f7eb5271bc5ab9ecbc23c
yousse-fmagdy <youssefmagdy1676@gmail.com>
2022-12-23 19:15:12 +0200

    create item

---
+++ lib/model/item.dart
@@ -0,0 +1,23 @@
+class Item {
+  String imgPath;
+  int price;
+  Item({required this.imgPath, required this.price});
+}
+
+
+final List<Item> items = [
+    Item(price: 80, imgPath: "assets/image/1.jpg"),
+    Item(price: 90, imgPath: "assets/image/2.jpg"),
+    Item(price: 100, imgPath: "assets/image/3.jpg"),
+    Item(price: 125, imgPath: "assets/image/4.jpg"),
+    Item(price: 60, imgPath: "assets/image/5.jpg"),
+    Item(price: 55, imgPath: "assets/image/6.jpg"),
+    Item(price: 60, imgPath: "assets/image/7.jpg"),
+    Item(price: 45, imgPath: "assets/image/8.jpg"),
+    Item(price: 70, imgPath: "assets/image/9.jpg"),
+    Item(price: 105, imgPath: "assets/image/10.jpg"),
+    Item(price: 30, imgPath: "assets/image/11.jpg"),
+    Item(price: 150, imgPath: "assets/image/12.jpg"),
+
+
+ ];
