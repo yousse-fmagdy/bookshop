db06ced3e0fd1efedacff6a3975534bb5d19a3b1
yousse-fmagdy <youssefmagdy1676@gmail.com>
2022-12-23 23:16:10 +0200

    create List selectedproduct

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -3,5 +3,15 @@
 
 
 
+List selectedproduct=[
+ Item(price: 80, imgPath: "assets/image/1.jpg", Location: "youssef book store"),
+   Item(price: 90, imgPath: "assets/image/2.jpg"),
+
+
+
+]
+
+
+
 
 
