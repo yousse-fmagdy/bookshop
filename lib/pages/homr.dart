45e1a6227112e8477bc579bbb9ce7d4f982826a5
yousse-fmagdy <youssefmagdy1676@gmail.com>
2022-12-23 17:47:01 +0200

    add gridDelegate

--- lib/pages/homr.dart
+++ lib/pages/homr.dart
@@ -9,104 +9,115 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
- child: Column(
+        body: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 33),
+            itemCount: 16,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                children: Stack(
+                  child: Positioned(
+                    right: 0,
+                    left: 0,
+                    
+                    child: ClipRRect(
+                                borderRadius: BorderRadius.circular(55),
+                                 child:Image.asset("assets/image/mecha.jpg") ,
+                    ),
+                  ),
+                ),
+                
+              );
+            }),
+
+
+
+        drawer: Drawer(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
- 
-              children: [
+            children: [
               Column(
                 children: [
- UserAccountsDrawerHeader(
-   decoration: BoxDecoration(
- image: DecorationImage(
- image: AssetImage("assets\image\profile.JPG"),
- fit: BoxFit.cover),
- ),
- currentAccountPicture: CircleAvatar(
- radius: 55,
- backgroundImage: AssetImage("assets\image\back.jpg")),
- 
-  accountEmail: Text("joe@gmail.com"),
-  accountName: Text("youssef magdy",
- style: TextStyle(
- color: Color.fromARGB(235, 12, 0, 0),
- )),
+                  UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets\image\profile.JPG"),
+                          fit: BoxFit.cover),
+                    ),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: AssetImage("assets\image\back.jpg")),
+                    accountEmail: Text("joe@gmail.com"),
+                    accountName: Text("youssef magdy",
+                        style: TextStyle(
+                          color: Color.fromARGB(235, 12, 0, 0),
+                        )),
+                  ),
+                  ListTile(
+                      title: Text("Home"),
+                      leading: Icon(Icons.home),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("My products"),
+                      leading: Icon(Icons.add_shopping_cart),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("About"),
+                      leading: Icon(Icons.help_center),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("Logout"),
+                      leading: Icon(Icons.exit_to_app),
+                      onTap: () {}),
+                ],
               ),
-
-  
-   ListTile(
-   title: Text("Home"),
-   leading: Icon(Icons.home),
-   onTap: () { }
- ),
-  
- ListTile(
-   title: Text("My products"),
-   leading: Icon(Icons.add_shopping_cart),
-   onTap: () { }
- ),
-
- ListTile(
-   title: Text("About"),
-   leading: Icon(Icons.help_center),
-   onTap: () { }
- ),
-
- ListTile(
-   title: Text("Logout"),
-   leading: Icon(Icons.exit_to_app),
-   onTap: () { }),
- 
-  
- ],
- ),
-  Container(
+              Container(
                 margin: EdgeInsets.only(bottom: 12),
                 child: Text("Developed by youusef magdy © 2022",
                     style: TextStyle(fontSize: 16)),
-  )
- ],
-      ),
- ),
-
-          
-     
-      
-      appBar: AppBar(
-        // ignore: prefer_const_literals_to_create_immutables
-        actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                      child: Text(
-                        "8",
-                        style: TextStyle(
-                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
-                      ),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(211, 164, 255, 193),
-                          shape: BoxShape.circle)),
-                  IconButton(
-                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 15),
-                child: Text(
-                  "\$ 128",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
+              )
             ],
           ),
-        ],
+        ),
+        appBar: AppBar(
+          // ignore: prefer_const_literals_to_create_immutables
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                        child: Text(
+                          "8",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(211, 164, 255, 193),
+                            shape: BoxShape.circle)),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: Text(
+                    "\$ 128",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ],
 
-        backgroundColor: appbarbrown,
+          backgroundColor: appbarbrown,
 
-        title: Text("Home"),
-      ));
+          title: Text("Home"),
+        ));
   }
 }
